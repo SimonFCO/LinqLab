a7a0dcc66610735f8db2f5581c7fbabd31b4559d
simonFCO <simon.vare6@gmail.com>
2026-04-11 14:30:32 +0200

    Add project files.

---
+++ LinqLab/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqLab
+{
+    internal class Menu
+    {
+        public static void RunProgram()
+        {
+
+        }
+    }
+}
