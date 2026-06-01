e3c20bee7e3e67148cead124e5697d6b80d23c5a
simonFCO <simon.vare6@gmail.com>
2026-06-01 20:28:53 +0200

    GetSuppliersWithLess10Products() Completed

--- LinqLab/Commands.cs
+++ LinqLab/Commands.cs
@@ -12,7 +12,7 @@ namespace LinqLab
         private readonly StoreDbContext _ctx;
         public Commands(StoreDbContext context)
         {
-            _ctx = context;   
+            _ctx = context;
         }
 
         public void GetElectronics()
@@ -25,37 +25,58 @@ namespace LinqLab
                     p.Name,
                     p.Price
                 });
-  
-            foreach(var product in Electronics)
+
+            foreach (var product in Electronics)
             {
                 Console.WriteLine($"Name: {product.Name} | Price: {product.Price}");
             }
-                  
+
         }
 
         public void GetSuppliersWithLess10Products()
         {
-           
+            var Suppliers = _ctx.Products
+                 .Where(p => p.StockQuantity < 10)
+                 .GroupBy(p => p.Supplier)
+                 .Select(g => new
+                 {
+                     g.Key.Name,
+                     products = g.Select(p => new
+                     {
+                         p.Name,
+                         p.StockQuantity
+                     })
+                 });
+
+            foreach(var supplier in Suppliers)
+            {
+                Console.WriteLine($"Supplier Name: {supplier.Name}");
+                foreach(var product in supplier.products)
+                {
+                    Console.WriteLine($"--- Product Name: {product.Name} | Quantity: {product.StockQuantity}");
+                }
+            }
+
         }
 
         public void GetTotalOrderValueLastMonth()
         {
-                   
+
         }
 
         public void GetTop3BestSellingProducts()
         {
-                   
+
         }
 
         public void GetCategoriesWithProductCount()
         {
-                   
+
         }
 
         public void GetHighValueOrdersWithDetails()
         {
 
-        }     
+        }
     }
 }
